3d1ce0011aeb415d15c8dc6430eb7d23ae2a96dd
_mukilesh <mukileshjs@outlook.com>
2024-01-25 16:30:23 +0530

    Newfiles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import React from "react"
+import Layout from "./Layout"
+import Home from "./Home"
+import About from "./About"
+import Contact from "./Contact"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="contact" element={<Contact/>}/>
+
+
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    
+  )
 }
 
-export default App;
+// import React from 'react'
+// import Basic from './Basic'
+// export default function App() {
+//   return (
+//     <div><Basic/></div>
+//   )
+// }
+
+// import React from "react";
+// import Counter from "./Counter"
+// export default function App()
+// {
+//   return(
+//   <div><Counter/></div>
+//   )
+// }
